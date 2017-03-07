3b0aafa87f1cb48e9083449f948d8c6939734474
Peter Sýs <pspps@medusa.medusa>
2017-03-07 23:28:59 +0000

    Add macros for file and path manipulation

--- security/medusa/l1/medusa.c
+++ security/medusa/l1/medusa.c
@@ -64,6 +64,22 @@
 
 #ifdef CONFIG_SECURITY_MEDUSA
 
+
+
+#define MEDUSA_DENTRY_PTR_TO_INODE_PTR(dentry) ((dentry)->d_inode)
+
+#define MEDUSA_PATH_PTR_TO_DENTRY_PTR(path) ((path)->dentry)
+#define MEDUSA_PATH_PTR_TO_INODE_PTR(path) MEDUSA_DENTRY_PTR_TO_INODE_PTR(MEDUSA_PATH_PTR_TO_DENTRY_PTR(path))
+#define MEDUSA_PATH_PTR_TO_FS_PTR(path) ((path)->mnt)
+
+#define MEDUSA_FILE_PTR_TO_INODE_PTR(file) ((file)->f_inode)
+#define MEDUSA_FILE_PTR_TO_PATH_PTR(file) ((file)->f_file)
+#define MEDUSA_FILE_PTR_TO_DENTRY_PTR(file) MEDUSA_PATH_PTR_TO_DENTRY_PTR(MEDUSA_FILE_PTR_TO_PATH_PTR(file))
+#define MEDUSA_FILE_PTR_TO_FS_PTR(file) MEDUSA_PATH_PTR_TO_FS_PTR(MEDUSA_FILE_PTR_TO_PATH_PTR(file))
+
+
+
+
 int medusa_l1_cred_alloc_blank(struct cred *cred, gfp_t gfp);
 int medusa_l1_inode_alloc_security(struct inode *inode);
 
