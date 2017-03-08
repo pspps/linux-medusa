61b39e437ba068acaeec9416f194724ae174bde9
Peter Sýs <pspps@medusa.medusa>
2017-03-08 16:20:15 +0000

    Reorder hooks

--- security/medusa/l1/medusa.c
+++ security/medusa/l1/medusa.c
@@ -78,79 +78,8 @@
 #define MEDUSA_FILE_PTR_TO_FS_PTR(file) MEDUSA_PATH_PTR_TO_FS_PTR(MEDUSA_FILE_PTR_TO_PATH_PTR(file))
 
 
-
-
-int medusa_l1_cred_alloc_blank(struct cred *cred, gfp_t gfp);
-int medusa_l1_inode_alloc_security(struct inode *inode);
-
-
-static int medusa_l1_sb_kern_mount(struct super_block *sb, int flags, void *data)
-{
-	struct dentry *root = sb->s_root;
-	struct inode *inode = root->d_inode;
-
-	if (&inode_security(inode) == NULL)
-		medusa_l1_inode_alloc_security(inode);
-
-	return 0;
-}
-
-static int medusa_l1_sb_set_mnt_opts(struct super_block *sb,
-				struct security_mnt_opts *opts,
-								unsigned long kern_flags,
-								unsigned long *set_kern_flags)
-{
-	if (unlikely(opts->num_mnt_opts))
-		return -EOPNOTSUPP;
-	return 0;
-}
-
-static int medusa_l1_dentry_init_security(struct dentry *dentry, int mode, 
-					const struct qstr *name, void **ctx, u32 *ctxlen)
-{
-	if (dentry->d_inode != NULL) {
-		if (&inode_security(dentry->d_inode) == NULL)
-			medusa_l1_inode_alloc_security(dentry->d_inode);
-
-	}
-	return -EOPNOTSUPP;
-}
-
-int medusa_l1_inode_alloc_security(struct inode *inode)
-{
-	struct medusa_l1_inode_s *med;
-
-	med = (struct medusa_l1_inode_s*) kmalloc(sizeof(struct medusa_l1_inode_s), GFP_KERNEL);
-
-	if (med == NULL)
-		return -ENOMEM;
-
-	inode->i_security = med;
-	medusa_clean_inode(inode);
-
-	return 0;
-}
-
-void medusa_l1_inode_free_security(struct inode *inode)
-{
-		struct medusa_l1_inode_s *med;
-
-	if (inode->i_security != NULL) {
-		med = inode->i_security;
-		inode->i_security = NULL;
-		kfree(med);
-	}
-}
-
-static int medusa_l1_inode_init_security(struct inode *inode, struct inode *dir,
-									const struct qstr *qstr, const char **name,
-									void **value, size_t *len)
-{
-	medusa_clean_inode(inode);
-
-	return 0;
-}
-
+//THIS HOOKS NEED TO BE REMOVED
+#ifndef MEDUSA_INODE_RELEASED
 static int medusa_l1_inode_create(struct inode *inode, struct dentry *dentry,
 				umode_t mode)
 {
@@ -223,10 +152,110 @@ static int medusa_l1_inode_readlink(struct dentry *dentry)
 	return 0;
 }
 
+static int medusa_l1_inode_setxattr(struct dentry *dentry, const char *name,
+					const void *value, size_t size, int flags)
+{
+	return cap_inode_setxattr(dentry, name, value, size, flags);
+} 
+
+
+static int medusa_l1_inode_removexattr(struct dentry *dentry, const char *name)
+{
+	return cap_inode_removexattr(dentry, name);
+} 
+
+static int medusa_l1_inode_need_killpriv(struct dentry *dentry) 
+{
+	return cap_inode_need_killpriv(dentry);
+}
+
+static int medusa_l1_inode_killpriv(struct dentry *dentry)
+{
+	return cap_inode_killpriv(dentry);
+}
+#endif
+//END OF HOOKS FOR REPLACE
+
+
+int medusa_l1_cred_alloc_blank(struct cred *cred, gfp_t gfp);
+int medusa_l1_inode_alloc_security(struct inode *inode);
+
+int medusa_l1_inode_alloc_security(struct inode *inode)
+{
+	struct medusa_l1_inode_s *med;
+
+	med = (struct medusa_l1_inode_s*) kmalloc(sizeof(struct medusa_l1_inode_s), GFP_KERNEL);
+
+	if (med == NULL)
+		return -ENOMEM;
+
+	inode->i_security = med;
+	medusa_clean_inode(inode);
+
+	return 0;
+}
+
+void medusa_l1_inode_free_security(struct inode *inode)
+{
+		struct medusa_l1_inode_s *med;
+
+	if (inode->i_security != NULL) {
+		med = inode->i_security;
+		inode->i_security = NULL;
+		kfree(med);
+	}
+}
+
+static int medusa_l1_inode_init_security(struct inode *inode, struct inode *dir,
+									const struct qstr *qstr, const char **name,
+									void **value, size_t *len)
+{
+	medusa_clean_inode(inode);
+
+	return 0;
+}
+
+static int medusa_l1_mmap_addr(unsigned long addr) 
+{
+	return cap_mmap_addr(addr);
+}
+
+static int medusa_l1_sb_kern_mount(struct super_block *sb, int flags, void *data)
+{
+	struct dentry *root = sb->s_root;
+	struct inode *inode = root->d_inode;
+
+	if (&inode_security(inode) == NULL)
+		medusa_l1_inode_alloc_security(inode);
+
+	return 0;
+}
+
+static int medusa_l1_sb_set_mnt_opts(struct super_block *sb,
+				struct security_mnt_opts *opts,
+								unsigned long kern_flags,
+								unsigned long *set_kern_flags)
+{
+	if (unlikely(opts->num_mnt_opts))
+		return -EOPNOTSUPP;
+	return 0;
+}
+
+static int medusa_l1_dentry_init_security(struct dentry *dentry, int mode, 
+					const struct qstr *name, void **ctx, u32 *ctxlen)
+{
+	if (dentry->d_inode != NULL) {
+		if (&inode_security(dentry->d_inode) == NULL)
+			medusa_l1_inode_alloc_security(dentry->d_inode);
+
+	}
+	return -EOPNOTSUPP;
+}
+
 static int medusa_l1_task_create(unsigned long clone_flags)
 {
-        if(medusa_fork(clone_flags) == MED_NO)
-                return -EPERM;   
+if(medusa_fork(clone_flags) == MED_NO)
+return -EPERM;   
 	return 0;
 }
 
@@ -285,7 +314,7 @@ static int medusa_l1_cred_prepare(struct cred *new, const struct cred *old, gfp_
 static int medusa_l1_task_kill(struct task_struct *p, struct siginfo *info,
 			 int sig, u32 secid)
 {
-    if(medusa_sendsig(sig, info, p) == MED_NO)
+if(medusa_sendsig(sig, info, p) == MED_NO)
 		return -EPERM;
 	return 0;
 }
@@ -294,34 +323,6 @@ static int medusa_l1_task_fix_setuid(struct cred *new, const struct cred *old,
 								int flags)
 {
 	return cap_task_fix_setuid(new, old, flags);
-}
-	
-
-static int medusa_l1_inode_setxattr(struct dentry *dentry, const char *name,
-					const void *value, size_t size, int flags)
-{
-	return cap_inode_setxattr(dentry, name, value, size, flags);
-} 
-
-
-static int medusa_l1_inode_removexattr(struct dentry *dentry, const char *name)
-{
-	return cap_inode_removexattr(dentry, name);
-} 
-
-static int medusa_l1_inode_need_killpriv(struct dentry *dentry) 
-{
-	return cap_inode_need_killpriv(dentry);
-}
-
-static int medusa_l1_inode_killpriv(struct dentry *dentry)
-{
-	return cap_inode_killpriv(dentry);
-}
-
-static int medusa_l1_mmap_addr(unsigned long addr) 
-{
-	return cap_mmap_addr(addr);
 }
 
 static int medusa_l1_task_setnice(struct task_struct *p, int nice)
@@ -906,27 +907,27 @@ struct security_hook_list medusa_l1_hooks_special[] = {
 void __init medusa_init(void);
 
 static void medusa_l1_init_sb(struct super_block *sb, void *unused) {
-        struct list_head *tmp;
-        struct inode *entry;
+struct list_head *tmp;
+struct inode *entry;
 
 	medusa_l1_sb_kern_mount(sb, 0, NULL);
 	printk("medusa: sb: %s\n", sb->s_root->d_name.name);
 
-        tmp = &sb->s_inodes;
-        list_for_each(tmp, &sb->s_inodes) {
-                entry = list_entry(tmp, struct inode, i_sb_list);
-                if (&inode_security(entry)==NULL) {
-                        medusa_l1_inode_alloc_security(entry);
-                }
-        }
+tmp = &sb->s_inodes;
+list_for_each(tmp, &sb->s_inodes) {
+entry = list_entry(tmp, struct inode, i_sb_list);
+if (&inode_security(entry)==NULL) {
+medusa_l1_inode_alloc_security(entry);
+}
+}
 }
 
 // Number and order of hooks has to be the same
 void security_replace_hooks(struct security_hook_list *old_hooks, struct security_hook_list *new_hooks, int count)
 {
-    int i;
-    for (i = 0; i < count; i++)
-        list_replace_rcu(&old_hooks[i].list, &new_hooks[i].list);
+int i;
+for (i = 0; i < count; i++)
+list_replace_rcu(&old_hooks[i].list, &new_hooks[i].list);
 }
 
 static int __init medusa_l1_init(void)
@@ -940,42 +941,42 @@ static int __init medusa_l1_init(void)
 	
 	security_add_hooks(medusa_l1_hooks, ARRAY_SIZE(medusa_l1_hooks));
 	printk("medusa: l1 registered with the kernel\n");
-    
-    security_replace_hooks(medusa_l0_hooks, medusa_l1_hooks_special, ARRAY_SIZE(medusa_l1_hooks_special));
 
-    extern bool l1_initialized;
-    extern struct cred_list l0_cred_list;
-    extern struct inode_list l0_inode_list;
-    extern struct mutex l0_mutex;
+security_replace_hooks(medusa_l0_hooks, medusa_l1_hooks_special, ARRAY_SIZE(medusa_l1_hooks_special));
 
-    mutex_lock(&l0_mutex);
+extern bool l1_initialized;
+extern struct cred_list l0_cred_list;
+extern struct inode_list l0_inode_list;
+extern struct mutex l0_mutex;
 
-    struct list_head *pos, *q;
-    struct inode_list *tmp;
-    struct cred_list *tmp_cred;
+mutex_lock(&l0_mutex);
 
-    list_for_each_safe(pos, q, &l0_inode_list.list) {
-        tmp = list_entry(pos, struct inode_list, list);
-        medusa_l1_inode_alloc_security(tmp->inode);
-        // printk("medusa: l1_alloc_security for an entry in the l0 list");
-        list_del(pos);
-        kfree(tmp); 
-    }
+struct list_head *pos, *q;
+struct inode_list *tmp;
+struct cred_list *tmp_cred;
 
-    list_for_each_safe(pos, q, &l0_cred_list.list) {
-        tmp_cred = list_entry(pos, struct cred_list, list);
-        medusa_l1_cred_alloc_blank(tmp_cred->cred, tmp_cred->gfp);
-        // printk("medusa: l1_cred_allo_blank for an entry in the l0 list");
-        list_del(pos);
-        kfree(tmp); 
-    }
+list_for_each_safe(pos, q, &l0_inode_list.list) {
+tmp = list_entry(pos, struct inode_list, list);
+medusa_l1_inode_alloc_security(tmp->inode);
+// printk("medusa: l1_alloc_security for an entry in the l0 list");
+list_del(pos);
+kfree(tmp); 
+}
 
-    l1_initialized = true;
+list_for_each_safe(pos, q, &l0_cred_list.list) {
+tmp_cred = list_entry(pos, struct cred_list, list);
+medusa_l1_cred_alloc_blank(tmp_cred->cred, tmp_cred->gfp);
+// printk("medusa: l1_cred_allo_blank for an entry in the l0 list");
+list_del(pos);
+kfree(tmp); 
+}
 
-    mutex_unlock(&l0_mutex);
+l1_initialized = true;
+
+mutex_unlock(&l0_mutex);
 
 	medusa_init();
-    
+
 	for_each_process(process) {
 		struct medusa_l1_task_s* med;
 		struct cred* tmp;
@@ -1013,4 +1014,3 @@ static void __exit medusa_l1_exit (void)
 module_init (medusa_l1_init);
 MODULE_LICENSE("GPL");
 #endif /* CONFIG_SECURITY_MEDUSA */
-
