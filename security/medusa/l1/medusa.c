3c49cff54769ed4c1f3cf517ce21c879e637a0fc
Peter Sýs <pspps@medusa.medusa>
2017-03-19 20:41:49 +0000

    path prvy pokus

--- security/medusa/l1/medusa.c
+++ security/medusa/l1/medusa.c
@@ -66,19 +66,6 @@
 #ifdef CONFIG_SECURITY_MEDUSA
 
 
-
-#define MEDUSA_DENTRY_PTR_TO_INODE_PTR(dentry) ((dentry)->d_inode)
-
-#define MEDUSA_PATH_PTR_TO_DENTRY_PTR(path) ((path)->dentry)
-#define MEDUSA_PATH_PTR_TO_INODE_PTR(path) MEDUSA_DENTRY_PTR_TO_INODE_PTR(MEDUSA_PATH_PTR_TO_DENTRY_PTR(path))
-#define MEDUSA_PATH_PTR_TO_FS_PTR(path) ((path)->mnt)
-
-#define MEDUSA_FILE_PTR_TO_INODE_PTR(file) ((file)->f_inode)
-#define MEDUSA_FILE_PTR_TO_PATH_PTR(file) ((file)->f_file)
-#define MEDUSA_FILE_PTR_TO_DENTRY_PTR(file) MEDUSA_PATH_PTR_TO_DENTRY_PTR(MEDUSA_FILE_PTR_TO_PATH_PTR(file))
-#define MEDUSA_FILE_PTR_TO_FS_PTR(file) MEDUSA_PATH_PTR_TO_FS_PTR(MEDUSA_FILE_PTR_TO_PATH_PTR(file))
-
-
 //THIS HOOKS NEED TO BE REMOVED
 #ifndef MEDUSA_INODE_RELEASED
 static int medusa_l1_inode_create(struct inode *inode, struct dentry *dentry,
